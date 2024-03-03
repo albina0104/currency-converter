744393a3a0332254f749df68b2527ef241e308eb
Albina Salkayeva <salkaeva.a@gmail.com>
2024-03-03 15:32:56 +0500

    Add test: testGetConversionRates_FileExistsAndUpToDate

---
+++ src/test/java/com/albinasalkayeva/currency_converter/ExchangeRateConnectorTest.java
@@ -0,0 +1,54 @@
+package com.albinasalkayeva.currency_converter;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ExchangeRateConnectorTest {
+    @Mock
+    JsonAndFileHelper jsonAndFileHelper;
+
+    @Test
+    public void testGetConversionRates_FileExistsAndUpToDate() throws IOException {
+        // Given
+        String baseCurrencyCode = "USD";
+        String targetCurrencyCode = "KZT";
+        BigDecimal conversionRate = BigDecimal.valueOf(450.7569);
+        String fileName = "conversion_rates_" + baseCurrencyCode + ".json";
+        ExchangeRateConnector connector = new ExchangeRateConnector(jsonAndFileHelper);
+        Date currentDate = new Date();
+        int secondsInFuture = 5;
+        long timestampInFutureUnix = currentDate.getTime() / 1000 + secondsInFuture;
+
+        when(jsonAndFileHelper.isFileExists(fileName)).thenReturn(true);
+        JSONObject mockJson = new JSONObject();
+        mockJson.put("time_next_update_unix", timestampInFutureUnix);
+        mockJson.put("conversion_rates", new JSONObject().put(targetCurrencyCode, conversionRate));
+        when(jsonAndFileHelper.getJsonFromFile(fileName)).thenReturn(mockJson);
+
+        // When
+        Map<String, BigDecimal> result = connector.getConversionRates(baseCurrencyCode);
+
+        // Then
+        InOrder inOrder = inOrder(jsonAndFileHelper);
+        inOrder.verify(jsonAndFileHelper, times(1)).isFileExists(fileName);
+        inOrder.verify(jsonAndFileHelper, times(1)).getJsonFromFile(fileName);
+        verify(jsonAndFileHelper, never()).getJsonFromUrl(any());
+        verify(jsonAndFileHelper, never()).saveJsonToFile(any(), any());
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey(targetCurrencyCode));
+        assertEquals(conversionRate, result.get(targetCurrencyCode));
+    }
+}
