83e458b2e18e92fb4463c7c02a5b175005c3e96e
Albina Salkayeva <salkaeva.a@gmail.com>
2024-03-05 20:44:32 +0500

    Add test: testGetConversionRates_FileExistsButNeedsUpdate

--- src/test/java/com/albinasalkayeva/currency_converter/ExchangeRateConnectorTest.java
+++ src/test/java/com/albinasalkayeva/currency_converter/ExchangeRateConnectorTest.java
@@ -51,4 +51,44 @@ public class ExchangeRateConnectorTest {
         assertTrue(result.containsKey(targetCurrencyCode));
         assertEquals(conversionRate, result.get(targetCurrencyCode));
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/conversionRates2.csv")
+    public void testGetConversionRates_FileExistsButNeedsUpdate(String baseCurrencyCode, String targetCurrencyCode,
+                                                                BigDecimal conversionRateBeforeUpdate,
+                                                                BigDecimal conversionRateAfterUpdate) throws IOException {
+        // Given
+        String fileName = "conversion_rates_" + baseCurrencyCode + ".json";
+        ExchangeRateConnector connector = new ExchangeRateConnector(jsonAndFileHelper);
+        Date currentDate = new Date();
+        int seconds = 5;
+        long timestampInPastUnix = currentDate.getTime() / 1000 - seconds;
+        long timestampInFutureUnix = currentDate.getTime() / 1000 + seconds;
+
+        when(jsonAndFileHelper.isFileExists(fileName)).thenReturn(true);
+
+        JSONObject mockJsonBeforeUpdate = new JSONObject();
+        mockJsonBeforeUpdate.put("time_next_update_unix", timestampInPastUnix);
+        mockJsonBeforeUpdate.put("conversion_rates", new JSONObject().put(targetCurrencyCode, conversionRateBeforeUpdate));
+
+        JSONObject mockJsonAfterUpdate = new JSONObject();
+        mockJsonAfterUpdate.put("time_next_update_unix", timestampInFutureUnix);
+        mockJsonAfterUpdate.put("conversion_rates", new JSONObject().put(targetCurrencyCode, conversionRateAfterUpdate));
+
+        when(jsonAndFileHelper.getJsonFromFile(fileName)).thenReturn(mockJsonBeforeUpdate);
+        when(jsonAndFileHelper.getJsonFromUrl(any())).thenReturn(mockJsonAfterUpdate);
+
+        // When
+        Map<String, BigDecimal> result = connector.getConversionRates(baseCurrencyCode);
+
+        // Then
+        InOrder inOrder = inOrder(jsonAndFileHelper);
+        inOrder.verify(jsonAndFileHelper, times(1)).isFileExists(fileName);
+        inOrder.verify(jsonAndFileHelper, times(1)).getJsonFromFile(fileName);
+        inOrder.verify(jsonAndFileHelper, times(1)).getJsonFromUrl(any());
+        inOrder.verify(jsonAndFileHelper, times(1)).saveJsonToFile(any(), any());
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey(targetCurrencyCode));
+        assertEquals(conversionRateAfterUpdate, result.get(targetCurrencyCode));
+    }
 }
